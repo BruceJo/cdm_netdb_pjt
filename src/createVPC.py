38ab7eb826a602ec240c45a9b515bd23a6ef349d
yuna <rud5356@naver.com>
2024-01-04 14:36:44 +0900

    yuna
    update

--- src/createVPC.py
+++ src/createVPC.py
@@ -51,7 +51,9 @@ class Create():
         dict1 = {}
         for key in self.include_keys[self.table_name]:
             ### step.4 Source 테이블에서 가져온 정보를 알맞게 변환
-            if key[-4:] == 'Name':      # 테스트 환경에서 Naver Cloud가 하나뿐이므로, 중복이름인경우 생성이 불가하기에 예외처리
+            if key == 'blockStorageSnapshotInstanceNo' or key =='snapshotTypeCode' or key == 'targetVpcName':
+                value = None
+            elif key[-4:] == 'Name':      # 테스트 환경에서 Naver Cloud가 하나뿐이므로, 중복이름인경우 생성이 불가하기에 예외처리
                 value = row_dict[key.lower()] + '-dr'
             elif key == 'vpcNo':
                 value = self.get_value('vpcno', 'vpc', **{'id' : row_dict['vpcid']})
@@ -63,8 +65,17 @@ class Create():
                 value = row_dict['blockstoragediskdetailtype']
             elif key == 'blockStorageVolumeTypeCode':
                 pass
-            elif key == 'blockStorageSnapshotInstanceNo':
-                value = None
+            elif key == 'blockStorageSize':
+                value = row_dict['blockstoragesize']
+                value = round(value/(1024**3))
+            elif key == 'originalBlockStorageInstanceNo':
+                value = row_dict['blockstoragesnapshotinstanceno']
+            elif key == 'sourceVpcNo':
+                value = self.get_value('vpcno', 'vpc', **{'id' : row_dict['sourcevpcid']})
+                print(value)
+            elif key == 'targetVpcNo':
+                value = self.get_value('vpcno', 'vpc', **{'id' : row_dict['targetvpcid']})
+                print(value)
             # 나중에 한번에 묶어 처리 'Code'
             elif key == 'loadBalancerTypeCode':
                 value = 'APPLICATION'   # 이 예제에서 db의 정보가 Code가 아닌 값으로 저장되어있어 예외처리
@@ -100,7 +111,7 @@ class Create():
 
     def run(self):
         ### for this in self.nc.keys():
-        this = 'publicipinstance' ### step.1 본인 Table을 기입
+        this = 'networkinterface' ### step.1 본인 Table을 기입
         try:
             self.set_url(this, "create")
         except KeyError:
