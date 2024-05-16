9586b3b7dc2afab00ccc6f9767093f1d47b89dd2
yuvnn <yubin11890@gmail.com>
2024-05-17 04:41:43 +0900

    set schemaName to StartTime

--- src/test.py
+++ src/test.py
@@ -1,17 +1,23 @@
 import connDbnApi as cda
 import getConfig as gcf
 import createSchema as cs
+import getHistory as gh
+import configparser as parser
+import os
+
+# basedir = os.path.dirname(os.path.abspath(__file__))
+# print("**********8",basedir)
+# os.chdir(basedir) #basedir 변경
 
 CONF_PATH = "../conf/app.conf"
 app_conf = gcf.Config(CONF_PATH).getConfig()
+destination = app_conf['DATABASE-SOURCE'].copy()
 
-
-def createDB():
+def CreateDB():
     destination = app_conf['DATABASE-SOURCE'].copy()
     conn = cda.Connect(db=destination).connect_cockroachdb()
     conn.autocommit = True
     cur = conn.cursor()
-
     q = f"SHOW DATABASES;"
     cur.execute(q)
     databases = cur.fetchall() #db의 이름을 리스트 (안에 요소는 튜플) 로 반환
@@ -19,23 +25,34 @@ def createDB():
     databaseName = []
     for database in databases:
         databaseName.append(database[0])
-    if 'cdm' not in databaseName:
-        q1 = f"CREATE DATABASE cdm;"
-        cur.execute(q1)
-        print("\ncreate cdm!")
+    if 'cdm' not in databaseName: #cdm db가 있는지 확인하고
+        q = f"CREATE DATABASE cdm;" #없다면 생성하기
+        cur.execute(q)
 
     cur.close()
     conn.close()
 
-def create() :
-    source = app_conf['DATABASE-SOURCE'].copy()
-    cock_create = cs.Create(source) 
-    cock_create.create_schema() 
-    cock_create.create_table()
-    return 'success'
+def CreateSchema():
+    #schemaName을 StartTime으로 세팅
+    StartTime = str(gh.SetStartTimestamp())
+    config = parser.ConfigParser()
+    config.read(CONF_PATH,encoding='utf-8')
+    config.set('DATABASE-SOURCE', 'schemaName', StartTime)
+    with open(CONF_PATH, 'w') as configfile:
+        config.write(configfile)
 
-# if __name__ == '__main__':
-#     #createDB()
+    # cock_create = cs.Create(source) 
+    # cock_create.create_schema() 
+    # cock_create.create_table()
+    # return 'success'
+
+
+# def create() :
+#     source = app_conf['DATABASE-SOURCE'].copy()
+#     
+if __name__ == '__main__':
+    CreateDB()
+    CreateSchema()
 
 
 
