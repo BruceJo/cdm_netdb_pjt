c37e568bc292a292d1ca3942bd846585b4db0be4
BruceJo <bruce1023@naver.com>
2024-05-21 20:36:47 +0900

    init Sync process(1)

--- src/createSchema.py
+++ src/createSchema.py
@@ -8,7 +8,8 @@ class Create():
         conn = cda.Connect(db = self.destination).connect_cockroachdb()
         conn.autocommit = True
         cur = conn.cursor()
-        q = f"CREATE SCHEMA IF NOT EXISTS {self.destination['schemaName']};"
+        # q = 'CREATE SCHEMA IF NOT EXISTS "'+self.destination["schemaName"]+'";'
+        q = f'CREATE SCHEMA IF NOT EXISTS "{self.destination["schemaName"]}";'
         cur.execute(q)
         cur.close()
         conn.close()
@@ -20,9 +21,10 @@ class Create():
         with open(self.destination["schemaPath"], 'r', encoding='UTF8') as file:
             sql = file.read()
         
-        sql = sql.replace("CREATE TABLE IF NOT EXISTS", f"CREATE TABLE IF NOT EXISTS {self.destination['schemaName']}.")
-        sql = sql.replace("CREATE SEQUENCE IF NOT EXISTS ", f"CREATE SEQUENCE IF NOT EXISTS {self.destination['schemaName']}.")
-        cur.execute(f"set schema {self.destination['schemaName']};")
+        sql = sql.replace('CREATE TABLE IF NOT EXISTS', f'CREATE TABLE IF NOT EXISTS "{self.destination["schemaName"]}".')
+        sql = sql.replace('CREATE SEQUENCE IF NOT EXISTS ', f'CREATE SEQUENCE IF NOT EXISTS "{self.destination["schemaName"]}".')
+        cur.execute(f'set schema "{self.destination["schemaName"]}";')
+        # cur.execute('set schema "'+self.destination['schemaName']+'";')
         cur.execute(sql)
         cur.close()
         conn.close()
